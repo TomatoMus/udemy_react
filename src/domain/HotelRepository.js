d14e6b9433e368c9a934148c0b1e111aeae0ed38
TomatoMus <sy12250@gmail.com>
2018-04-18 21:40:31 +0900

    first commit

---
+++ src/domain/HotelRepository.js
@@ -0,0 +1,35 @@
+import geolib from 'geolib';
+import Rakuten from '../lib/Rakuten';
+
+const RAKUTEN_APP_ID = '1079875006289887983';
+
+export const searchHotelByLocation = (location) => {
+  const params = {
+    applicationId: RAKUTEN_APP_ID,
+    datumType: 1,
+    latitude: location.lat,
+    longitude: location.lng,
+  };
+  return Rakuten.Travel.simpleHotelSearch(params)
+    .then(result =>
+      result.data.hotels.map((hotel) => {
+        const basicInfo = hotel.hotel[0].hotelBasicInfo;
+        // const price = basicInfo.hotelMinCharge;
+        const distance = geolib.getDistance(
+          { latitude: location.lat, longitude: location.lng },
+          { latitude: basicInfo.latitude, longitude: basicInfo.longitude },
+        );
+        return {
+          id: basicInfo.hotelNo,
+          name: basicInfo.hotelName,
+          url: basicInfo.hotelInformationUrl,
+          thumbUrl: basicInfo.hotelThumbnailUrl,
+          // price: price ? `${price} 円` : '空室なし',
+          price: basicInfo.hotelMinCharge,
+          reviewAverage: basicInfo.reviewAverage,
+          reviewCount: basicInfo.reviewCount,
+          distance,
+        };
+      }),
+    );
+};
